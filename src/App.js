7e98ec0b7f5c0a8fb7770f37c37e0c8df4088267
Diego Valenzuela <dvalenzuela@sanservices.hn>
2019-10-28 11:10:23 -0600

    Finished prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            username: '',
+            password: ''
+         };
+    }
+    onSubmitHandler = (event) => {
+        event.preventDefault();
+        if (this.state.username == 'admin' && this.state.password == 'password')
+            alert('Login Successful.');
+        else
+        {
+            alert('Incorrect username or password');
+            this.setState({
+                username: '',
+                password: ''
+            });
+        }
+    }
+    handleChange = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+    }
+    changeName = (newName) => {
+        this.setState({ username: newName.target.value});
+    }
+    render() {
+        return (
+            <form style={formStyle} onSubmit={this.onSubmitHandler}>
+                <h1 style={titleStyle}>Welcome to UNITEC: </h1>
+                <p style={instructionStyle}>Please enter your username: </p>
+                <p>
+                    Username: <input
+                    style={inputStyle}
+                    id="usernameInput"
+                    name="username"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.username}/>
+                </p>
+                <p>Password: 
+                    <input
+                    style={inputStyle}
+                    id="passInput"
+                    name="password"
+                    onChange={this.handleChange}
+                    type="password"
+                    value={this.state.password}
+                    />
+                </p>
+                <input style={ buttonStyle } type="submit"/>
+            </form>
+        )
+    }
 }
 
-export default App;
+const formStyle = {
+    margin: '6% 0 0 42%'
+}
+
+const buttonStyle = {
+    width: '10em',
+    height: '2em',
+    backgroundColor: 'cyan',
+    marginTop: '25px',
+    marginLeft: '7%'
+}
+
+const titleStyle = {
+    fontWeight: '800',
+    color: 'red'
+}
+
+const instructionStyle = {
+    fontWeight: '800',
+    marginBottom: '50px',
+    marginTop: '50px'
+}
+
+const inputStyle = {
+    width: '20em',
+    borderRadius: '10px',
+    transform: 'scale(1, 1.2)'
+}
+
+export default LoginForm;
